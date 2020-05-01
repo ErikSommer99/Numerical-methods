eb12232301d294c92eb6193ef7578ef0a076be24
Erik Storås Sommer <eriksommer99@gmail.com>
2020-05-01 13:29:41 +0200

    RiemannIntegral

---
+++ src/numerikk/RiemannIntegral.java
@@ -0,0 +1,44 @@
+package numerikk;
+
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+/**
+ * Skript som estimerer et integral
+ * med en Riemann sum.
+ */
+
+public class RiemannIntegral {
+
+    public static void main(String[] args) {
+        //Funksjon: sin(sqrt(x))
+
+        //Grense
+        double a = 1;
+        double b = 3;
+        double x;
+
+        //Oppdeling
+        Scanner in = new Scanner(System.in);
+        System.out.print("Antall delintervall: ");
+        int n = in.nextInt();
+
+        //Bredde av søyle
+        double dx = (b-a)/n;
+
+        //Ittererer x og summen R
+        double R = 0;
+        x = a + dx/2;
+
+        //Summerer
+        for (int i = 0; i < n; i++){
+            R = R + sin(sqrt(x)) * dx;
+            x = x + dx;
+        }
+
+        //Skriver ut svaaret
+        System.out.println(R);
+
+    }
+}
