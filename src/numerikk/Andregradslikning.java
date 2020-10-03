2c20857c4f7056bbe4a0dece4e27db162d3bcbb6
Erik Storås Sommer <eriksommer99@gmail.com>
2020-10-03 19:57:17 +0200

    Bug

--- src/numerikk/Andregradslikning.java
+++ src/numerikk/Andregradslikning.java
@@ -22,7 +22,6 @@ public class Andregradslikning {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        JF
 
         System.out.print("a: ");
         double a = in.nextDouble();
